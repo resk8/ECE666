abde7af430ab58ba62057a1bc6df375d3ff6fef5
resk8 <resk8.ro@gmail.com>
2021-02-09 04:03:20 -0800

    complete mpi matrix mul

--- Homework2/MatrixMul/mpi_matrixmul.c
+++ Homework2/MatrixMul/mpi_matrixmul.c
@@ -5,28 +5,23 @@
 #include <unistd.h>
 #include <mpi.h>
 
-#define NUM_PROCESSES 2
-#define DIM 1
+#define MASTER 0               /* taskid of first task */
+#define FROM_MASTER 1          /* setting a message type */
+#define FROM_WORKER 2          /* setting a message type */
+
+#define NUM_PROCESSES 6
+#define DIM 9
 
 int g_numThreads;
-//pthread_t * g_worker_threads;
-
-typedef struct MatMultData {
-    int _tid;
-    int _N;
-    int *_a;
-    int *_b;
-    int *_c;    
-} matMultData_t;
 
 void init_mat(int N, int *a, int *b, int *c) {
     int i, j, k;
 
     for(j=0; j<N; j++) {
         for(i=0; i<N; i++) {
-            a[i*N + j] = 1;
-            b[i*N + j] = 1;
-            c[i*N + j] = 0;
+            *((a+i*N) + j) = 1;
+            *((b+i*N) + j) = 1;
+            *((c+i*N) + j) = 0;
         }
     }    
 }
@@ -38,36 +33,17 @@ void some_matmult(int N, int *a, int *b, int *c) {
   for (i = 0; i < N; i++)	
     for (j = 0; j < N; j++)
       for (k = 0; k < N; k++)
-	    c[i*N + j] += a[i*N + k] * b[k*N + j];
+	    *((c+i*N) + j) += *((a+i*N) + k) * *((b+k*N) + j);
 }
 
-void * worker(void *args)
-{   
-    int chunk_size, start, end;
-    int i, j, k, sum;
-    matMultData_t *data = args;
-
-    //printf("Matrix multplication thread thread %d\n", data->_tid);
-    //split the matrix in chunks based on the amount of threads
-    chunk_size = data->_N / g_numThreads;
-    start = data->_tid  * chunk_size;
-    end = start + chunk_size;
-
-    for(i=start; i<end; i++) {
-        for(j=0; j<data->_N; j++) {
-            for(k=0; k<data->_N; k++) {
-                data->_c[i*data->_N + j] += data->_a[i*data->_N + k] * data->_b[k*data->_N + j];
-            }
-        }
-    }
-    return NULL;
-}
-
-void matmult(int N, int *a, int *b, int *c) {
-    int i, j, k, myid, start, end, chunk_size, worker, d[N*N], tmp;
+void matmult(int N) {
+    int i, j, k, myid, start, end, chunk_size, worker, d[N][N], tmp;
     MPI_Status work_status;
+    int a[N][N];
+    int b[N][N];
+    int c[N][N];
+    double tstart1, tend1, tstart2, tend2, exectime1, exectime2;
     g_numThreads = NUM_PROCESSES + 1; // 9 processors -> 8 workers, 1 main
-    chunk_size = N/NUM_PROCESSES;     // calculate the size of data a worker will work on
 
     MPI_Init(NULL, NULL);                 // Initialize the MPI environments
     MPI_Comm_rank(MPI_COMM_WORLD, &myid); // Get the rank of the process
@@ -75,77 +51,80 @@ void matmult(int N, int *a, int *b, int *c) {
         
         printf("\nStarting Matrix Multiplication Test...\n");
 
-        init_mat(N,a,b,c);
-        init_mat(N,a,b,d);        
+        init_mat(N,&a[0][0],&b[0][0],&c[0][0]);
+        init_mat(N,&a[0][0],&b[0][0],&d[0][0]);
 
+        // for (i=0;i<N;i++)
+        //     for (j=0; j<N; j++) 
+        //         printf("a[%d][%d] = %d, b[%d][%d] = %d\n", i,j, a[i][j],i,j,b[i][j]);
+
+        tstart1 = MPI_Wtime();
         //send work to workers
+        chunk_size = (N/NUM_PROCESSES);     // calculate the size of data a worker will work on
         for (worker = 1; worker < g_numThreads; worker++) {
-            start = (worker-1) * chunk_size; // worker 1 will start at address [0, chunk_size]
-            end = start + chunk_size;
-
-            printf("Sending to worker %d, Matrix a start 0x%08x, Matrix b start 0x%08x, start at %d in chunks of %d\n", worker, &a, &b, start, chunk_size);
+            start = (worker-1) * chunk_size;
             MPI_Send(&start, 1, MPI_INT, worker, 1, MPI_COMM_WORLD);
-            MPI_Send(&end, 1, MPI_INT, worker, 1, MPI_COMM_WORLD);
-            MPI_Send(&a[start], chunk_size*N, MPI_INT, worker, 1, MPI_COMM_WORLD);
+            MPI_Send(&chunk_size, 1, MPI_INT, worker, 1, MPI_COMM_WORLD);
+            MPI_Send(&a[start][0], chunk_size*N, MPI_INT, worker, 1, MPI_COMM_WORLD);
             MPI_Send(&b, N*N, MPI_INT, worker, 1, MPI_COMM_WORLD);
         }
 
         //receive results from workers
         for (worker = 1; worker < g_numThreads; worker++) {
             MPI_Recv(&start, 1, MPI_INT, worker, 2, MPI_COMM_WORLD, &work_status);
-            MPI_Recv(&end, 1, MPI_INT, worker, 2, MPI_COMM_WORLD, &work_status);
-            MPI_Recv(&c[start], chunk_size*N, MPI_INT, worker, 2, MPI_COMM_WORLD, &work_status);
+            MPI_Recv(&chunk_size, 1, MPI_INT, worker, 2, MPI_COMM_WORLD, &work_status);
+            MPI_Recv(&c[start][0], chunk_size*N, MPI_INT, worker, 2, MPI_COMM_WORLD, &work_status);
         }
+        tend1 = MPI_Wtime();
 
         //sequential
-        some_matmult(N,a,b,d);
+        tstart2 = MPI_Wtime();
+        some_matmult(N,&a[0][0],&b[0][0],&d[0][0]);
+        tend2 = MPI_Wtime();
+        
 
         char result[5];
         sprintf(result, "%s", "PASS");
-        for(i=0;i<N*N;i++)
+        for(i=0;i<N;i++)
         {
-            //printf("c[%d] = %d, d[%d] = %d\n",i,c[i],i,d[i]);
-            if(c[i] != d[i]) {
-                sprintf(result, "%s", "FAIL");
+            for(j=0;j<N;j++) {
+                //printf("c[%d][%d] = %d, d[%d][%d] = %d\n", i,j, c[i][j],i,j,d[i][j]);
+                if(c[i][j] != d[i][j]) {
+                    sprintf(result, "%s", "FAIL");
+                }
             }
         }
 
-        //exectime1 = (tend1.tv_sec - tstart1.tv_sec) * 1000.0; // sec to ms
-        //exectime1 += (tend1.tv_usec - tstart1.tv_usec) / 1000.0; // us to ms
+        exectime1 = (tend1 - tstart1) * 1000.0; // sec to ms
+        exectime1 += (tend1 - tstart1) / 1000.0; // us to ms
 
-        //exectime2 = (tend2.tv_sec - tstart2.tv_sec) * 1000.0; // sec to ms
-        //exectime2 += (tend2.tv_usec - tstart2.tv_usec) / 1000.0; // us to ms
+        exectime2 = (tend2 - tstart2) * 1000.0; // sec to ms
+        exectime2 += (tend2 - tstart2) / 1000.0; // us to ms
 
         printf("MPI Matrix mul. Result is %s\n", result);
-        //printf("Parallel MPI execution time %.3lf ms\n", exectime1);
-        //printf("Sequential execution time %.3lf ms\n", exectime2);
+        printf("Parallel MPI execution time %.3lf ms\n", exectime1);
+        printf("Sequential execution time %.3lf ms\n", exectime2);
 
     } else {        
         //receive data from main to start work
         MPI_Recv(&start, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &work_status);
-        MPI_Recv(&end, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &work_status);
+        MPI_Recv(&chunk_size, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &work_status);
         MPI_Recv(&a, chunk_size*N, MPI_INT, 0, 1, MPI_COMM_WORLD, &work_status);
         MPI_Recv(&b, N*N, MPI_INT, 0, 1, MPI_COMM_WORLD, &work_status);
-        //printf("worker %d receiving work from Main\n",myid);
 
-        printf("Receiving worker %d, Matrix a start 0x%08x, Matrix b start 0x%08x, start at %d in chunks of %d\n", myid, a, b, start, chunk_size);
-        //THE WORK
+        //the work
         for(i=0; i<chunk_size; i++) {
            for(j=0; j<N; j++) {
+               c[i][j] = 0;
                for(k=0; k<N; k++) {
-                   c[i*N + j] += a[i*N + k] * b[k*N + j];
-                   printf("c[%d] = %d, a[%d] = %d b[%d] = %d\n",i*N + j,c[i*N + j],i*N + k,a[i*N + k],k*N + j,b[k*N + j]);
-                   //tmp = a[i*N + k];
-                   //tmp = b[k*N + j];
-                   //c[i*N + j] = 1;
-                   
+                   c[i][j] += a[i][k] * b[k][j];
                }
            }
         }
 
         //send result to main
         MPI_Send(&start, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-        MPI_Send(&end, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+        MPI_Send(&chunk_size, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
         MPI_Send(&c, chunk_size*N, MPI_INT, 0, 2, MPI_COMM_WORLD);
     }
 
@@ -154,15 +133,6 @@ void matmult(int N, int *a, int *b, int *c) {
 }
 
 int main(void) {
-    int i;
-    //double exectime1, exectime2;
-    //struct timeval tstart1, tend1, tstart2, tend2;
-    
-    int dim = pow(2,DIM);
-    int twoD_dim = dim*dim; //2dmatrix N X N
-    int A[twoD_dim];
-    int B[twoD_dim];
-    int C[twoD_dim];
-    matmult(dim,A,B,C);
+    matmult(pow(2,DIM));
     return 0;
 }
