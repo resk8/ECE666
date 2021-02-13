540c1f6227b92efd27a4b48a8fd0818eaf4bf559
Renato Andrade De Oliveira <rolivie@ecegrid-thin1.ecn.purdue.edu>
2021-02-13 02:21:59 -0500

    MatrixMul working with Input and Output files

--- Homework2/MatrixMul/mpi_matrixmul.c
+++ Homework2/MatrixMul/mpi_matrixmul.c
@@ -51,18 +51,19 @@ int read_from_file(char * filename, double *data, int num) {
     }
 } 
 
-void init_mat(int N, double *a, double *b) {
-    int i, j;
 
-    srand48(time(NULL)); //Give drand48() a seed value
+// void init_mat(int N, double *a, double *b) {
+//     int i, j;
+
+//     srand48(time(NULL)); //Give drand48() a seed value
     
-    for(j=0; j<N; j++) {
-        for(i=0; i<N; i++) {
-            *((a+i*N) + j) = drand48(); // 1.0;
-            *((b+i*N) + j) = drand48(); // 1.0;
-        }
-    }    
-}
+//     for(j=0; j<N; j++) {
+//         for(i=0; i<N; i++) {
+//             *((a+i*N) + j) = drand48(); // 1.0;
+//             *((b+i*N) + j) = drand48(); // 1.0;
+//         }
+//     }    
+// }
 
 void sequential_matmult(int N, double *a, double *b, double *c) {
   int i, j, k;
@@ -79,15 +80,21 @@ void sequential_matmult(int N, double *a, double *b, double *c) {
 }
 
 int main(int argc, char* argv[]) {
-    int i, j, k, myid, start, end, chunk_size, worker, offset, N;
+    int i, j, k, myid, start, end, chunk_size, worker, offset, N, rval;
     double tstart1, tend1, tstart2, tend2, exectime1, exectime2;
     MPI_Status work_status;
+    char file_a[15];
+    char file_b[15];
+    char file_c[15];
 
     if(argc < 2) {
         printf("Need to pass matrix dimension\n");
         return -1;
     } else {
         N = atoi(argv[1]);
+        sprintf(file_a,"a.%d.bin",N);
+        sprintf(file_b,"b.%d.bin",N);
+        sprintf(file_c,"c.%d.bin",N);
     }
 
     double* a = (double*)malloc(N*N*sizeof(double));
@@ -101,12 +108,19 @@ int main(int argc, char* argv[]) {
     chunk_size = N/(g_numThreads);                //calculate the size of data a worker will work on
     //printf("rank = %d, number of p = %d\n", myid, g_numThreads);
     if (myid == 0) {
-        printf("Starting Matrix Multiplication Test...\n");
+        if ((read_from_file(file_a,a,N*N) != 1) || (read_from_file(file_b,b,N*N) != 1)) {
+#ifdef MYDEBUG
+            printf("could not read input files\n");
+#endif
+            goto end;
+        }
 
+#ifdef MYDEBUG
+        printf("Starting Parallel Matrix Multiplication Test...\n");
         //init matrices
-        init_mat(N,a,b);
-
+        //init_mat(N,a,b);
         tstart1 = MPI_Wtime();
+#endif
         //send work to workers
         for (worker = 1; worker < g_numThreads; worker++) {
             start = worker * chunk_size; //chunk of data starting address for worker
@@ -131,9 +145,11 @@ int main(int argc, char* argv[]) {
             offset = start * N;          //starting row of the chunk (convert from 1D to 2D)
             MPI_Recv((c+offset), chunk_size*N, MPI_DOUBLE, worker, 2, MPI_COMM_WORLD, &work_status);
         }
+#ifdef MYDEBUG
         tend1 = MPI_Wtime();
 
         //sequential
+        printf("Starting Sequential Matrix Multiplication Test...\n");
         tstart2 = MPI_Wtime();
         sequential_matmult(N,a,b,d);
         tend2 = MPI_Wtime();
@@ -144,7 +160,7 @@ int main(int argc, char* argv[]) {
         for(i=0;i<N;i++)
         {
             for(j=0;j<N;j++) {
-                printf("c[%d][%d] = %6.2f, d[%d][%d] = %6.2f\n", i,j, *((c+i*N) + j),i,j,*((d+i*N) + j));
+                //printf("c[%d][%d] = %6.2f, d[%d][%d] = %6.2f\n", i,j, *((c+i*N) + j),i,j,*((d+i*N) + j));
                 if(*((c+i*N) + j) != *((d+i*N) + j)) {
                     sprintf(result, "%s", "FAIL");
                 }
@@ -159,7 +175,32 @@ int main(int argc, char* argv[]) {
         printf("MPI Matrix mul. Result is %s\n", result);
         printf("Parallel MPI execution time %.3lf ms\n", exectime1);
         printf("Sequential execution time %.3lf ms\n", exectime2);
+#endif
+        //output data to file
+        rval = write_to_file(file_c,c,N*N);
+#ifdef MYDEBUG
+        if(rval == 1) {
+            printf("Successfully written output matrix.\n");
+            rval = read_from_file(file_c,d,N*N);
+        } else {
+            printf("Could not write output matrix\n");
+        }
 
+        if(rval == 1) {
+	        for(i=0; i<N*N; i++) {
+	            if (c[i] != d[i]) break;
+            }
+        
+            if(i==N*N) {
+                printf("integrity verified. All %d elements match.\n", N*N);
+            } else {
+                printf("Mismatch at element %d (a[%d] = %6.4f; b[%d] = %6.4f;)\n",
+                      i, i, c[i], i, d[i]);
+            }
+        } else {
+            printf("Could read output matrix for verification\n"); 
+        }
+#endif
     } else {        
         //receive data from main to start work
         MPI_Recv(a, chunk_size*N, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &work_status);
@@ -179,9 +220,9 @@ int main(int argc, char* argv[]) {
         MPI_Send(c, chunk_size*N, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
     }
 
+end:
     // Finalize the MPI environment.
     MPI_Finalize();
-
     free(a);
     free(b);
     free(c);
