c01cf1759f4d950aa1e10bed4a50ef6d527a8087
resk8 <resk8.ro@gmail.com>
2021-05-05 20:59:59 -0700

    adding graphs of pf vs non-pf

--- Parsec/utils/graph.py
+++ Parsec/utils/graph.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/opt/homebrew/bin/python3
 
 import sys
 import os
@@ -18,16 +18,24 @@ def main():
     """
     TODO
     """  
-    field_list=['simSeconds', 
-                'simInsts',
-                'system.ruby.network.msg_count.Request_Control',
-                'system.ruby.network.msg_count.Response_Data',
-                'system.ruby.network.msg_count.Response_Control',
-                'system.ruby.network.msg_count.Writeback_Data',
-                'system.ruby.network.msg_count.Writeback_Control',
-                'system.ruby.network.msg_count.Broadcast_Control',
-                'system.ruby.network.msg_count.Multicast_Control',
-                'system.ruby.network.msg_count.Unblock_Control']
+    # field_list=['simSeconds', 
+    #             'simInsts',
+    #             'system.ruby.network.msg_count.Request_Control',
+    #             'system.ruby.network.msg_count.Response_Data',
+    #             'system.ruby.network.msg_count.Response_Control',
+    #             'system.ruby.network.msg_count.Writeback_Data',
+    #             'system.ruby.network.msg_count.Writeback_Control',
+    #             'system.ruby.network.msg_count.Broadcast_Control',
+    #             'system.ruby.network.msg_count.Multicast_Control',
+    #             'system.ruby.network.msg_count.Unblock_Control']
+
+    field_list={'simSeconds':'seconds', 
+                'simInsts' : 'number of instructions',
+                'system.ruby.network.avg_link_utilization' : 'count',
+                'system.ruby.network.average_packet_latency' : 'count',
+                'system.ruby.network.average_packet_network_latency' : 'count',
+                'system.ruby.network.packets_injected::total' : 'packets',
+                'system.ruby.network.packets_received::total' : 'packets'}
 
     try:
         if not os.path.isdir(args.benchmark_dir):
@@ -52,66 +60,69 @@ def main():
                     if value and field:
                         if field[0] in field_list:
                             data[benchmark][dir_index].setdefault(field[0], float(value[0]))                            
-             
-        for field in field_list:
-            average_pf = 0 
-            average_no_pf = 0
-            difference = 0   
+    
+        with open("percentage.log", "w") as f: 
+            for field, units in field_list.items():
+                average_pf = 0 
+                average_no_pf = 0
+                difference = 0   
 
-            pf_field_list=[]
-            no_pf_field_list=[]
-            for BenMrk in data.keys():
-                pf_data = 0
-                no_pf_data = 0
-                try: #try block to check if field key exist in the dict                          
-                    pf_data = data[BenMrk]['pf_on'][field] 
+                pf_field_list=[]
+                no_pf_field_list=[]
+                for BenMrk in data.keys():
+                    pf_data = 0
+                    no_pf_data = 0
+                    try: #try block to check if field key exist in the dict                          
+                        pf_data = data[BenMrk]['pf_on'][field] 
+                    except:
+                        pass
+
+                    try: #try block to check if field key exist in the dict                          
+                        no_pf_data = data[BenMrk]['pf_off'][field]
+                    except:
+                        pass
+                    
+                    pf_field_list.append(pf_data)
+                    no_pf_field_list.append(no_pf_data)
+                    diff = abs(pf_data - no_pf_data)
+                    median = (pf_data + no_pf_data)/2         
+                    celling = median+3*diff
+                    floor = median-3*diff if median-3*diff > 0 else 0
+                    if field in ['simSeconds','simInsts']:
+                        title = field             
+                    else:
+                        title = field.rsplit('.',1)[1]                    
+
+                    average_pf +=  pf_data
+                    average_no_pf += no_pf_data
+
+                width = 0.2
+                plt.figure(figsize=(15,3))
+                plt.bar(data.keys(), no_pf_field_list, width=width, align='edge', label='no probe filtering')
+                plt.bar(data.keys(), pf_field_list, width=-width, align='edge', label='probe filtering')    
+                plt.xticks([r for r in range(len(list(data.keys())))],list(data.keys()))
+                plt.ylabel(units)
+                plt.ylim(floor,celling)
+                plt.title(title)        
+
+                plt.legend()
+                plt.savefig(field + '.png')
+                plt.show()
+
+                average_pf = average_pf/len(data.keys())    
+                average_no_pf = average_no_pf/len(data.keys())
+                try:
+                    if average_pf > average_no_pf:
+                        difference = (abs(average_pf - average_no_pf)/average_pf)*100
+                    else:
+                        difference = (abs(average_pf - average_no_pf)/average_no_pf)*100
                 except:
-                    pass
+                    difference = 0
 
-                try: #try block to check if field key exist in the dict                          
-                    no_pf_data = data[BenMrk]['pf_off'][field]
-                except:
-                    pass
-                
-                pf_field_list.append(pf_data)
-                no_pf_field_list.append(no_pf_data)
-                diff = abs(pf_data - no_pf_data)
-                median = (pf_data + no_pf_data)/2         
-                celling = median+3*diff
-                floor = median-3*diff if median-3*diff > 0 else 0
-                if field in 'simSeconds':
-                    title = field
-                    msg = 'seconds'                    
-                else:
-                    title = field.rsplit('.',1)[1]
-                    msg = 'count'
-
-                average_pf +=  pf_data
-                average_no_pf += no_pf_data
-
-            width = 0.2
-            plt.figure(figsize=(15,3))
-            plt.bar(data.keys(), no_pf_field_list, width=width, align='edge', label='no probe filtering')
-            plt.bar(data.keys(), pf_field_list, width=-width, align='edge', label='probe filtering')    
-            plt.xticks([r for r in range(len(list(data.keys())))],list(data.keys()))
-            plt.ylabel(msg)
-            plt.ylim(floor,celling)
-            plt.title(title)        
-
-            plt.legend()
-            plt.savefig(field + '.png')
-            plt.show()
-
-            average_pf = average_pf/len(data.keys())    
-            average_no_pf = average_no_pf/len(data.keys())
-            try:
-                if average_pf > average_no_pf:
-                    difference = (abs(average_pf - average_no_pf)/average_pf)*100
-                else:
-                    difference = (abs(average_pf - average_no_pf)/average_no_pf)*100
-            except:
-                difference = 0                
-            print("{} : average_pf {}, average_no_pf {}, difference percent {}".format(field, average_pf, average_no_pf, difference))
+                msg = "{} : average_pf {}, average_no_pf {}, difference percent {}".format(field, average_pf, average_no_pf, difference)
+                print(msg)
+                msg += '\n'
+                f.write(msg)
 
     except Exception as e:
         traceback.print_exc()
